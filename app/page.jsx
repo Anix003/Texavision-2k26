5f4059bf9e9376780bf12e52beada4381501b741
Sunanda-05 <mannasunanda11705@gmail.com>
2025-12-24 17:28:45 +0530

    refactor: unused imports removal

--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,6 @@ import Merch from '@/components/sections/home/merch/Merch';
 import Performances from '@/components/sections/home/performances/Performances';
 import Sponsors from '@/components/sections/home/sponsors/Sponsors';
 import Timeline from '@/components/sections/home/timeline/Timeline';
-import React from 'react';
-
 const Home = () => {
   return (
     <>
