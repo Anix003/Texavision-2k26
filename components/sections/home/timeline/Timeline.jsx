73f9933d1965cbc51d28a35a1bfd9ee52e2ae208
Sunanda-05 <mannasunanda11705@gmail.com>
2025-12-26 19:44:08 +0530

    feat: poster reveal brush animation

--- components/sections/home/timeline/Timeline.jsx
+++ components/sections/home/timeline/Timeline.jsx
@@ -1,5 +1,19 @@
+import Image from 'next/image';
+import BrushSwipeAnimation from './PosterReveal';
+import PosterBg from '@/assets/images/svgs/poster.svg';
+
 const Timeline = () => {
-  return <div>Timeline</div>;
+  return (
+    <div className="bg-sand relative overflow-hidden">
+      <div className="absolute top-0 left-0 z-0 h-full w-full">
+        <Image src={PosterBg} alt="Timeline" className="h-auto w-full" />
+      </div>
+
+      <div className="relative">
+        <BrushSwipeAnimation />
+      </div>
+    </div>
+  );
 };
 
 export default Timeline;
